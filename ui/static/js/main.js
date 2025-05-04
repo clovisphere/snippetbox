8ea76c6f728c892544878b871b8d1c1fd1b71b53
clovisphere <clovis.mugaruka@gmail.com>
2025-05-04 11:59:35 +0300

    Add templates + some css/js magic

---
+++ ui/static/js/main.js
@@ -0,0 +1,6 @@
+/* ui/static/js/main.js */
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Snippetbox loaded with Ubuntu Mono vibes.");
+  // Future enhancement: flash messages, snippet toggles, etc.
+});
